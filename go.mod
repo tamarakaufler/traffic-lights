cac2489a410e99a0b74867a26d451639c509d0e5
Tamara Kaufler <tkaufler@gmail.com>
2019-09-30 22:50:47 +0100

    adds more logging

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module tamarakaufler/traffic-lights
+module tamarakaufler/traffic-lights/v1
 
 go 1.12
 
-require github.com/stianeikeland/go-rpio v4.2.0+incompatible
+require github.com/stianeikeland/go-rpio/v4 v4.4.0
--- main.go
+++ main.go
@@ -7,13 +7,16 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/stianeikeland/go-rpio"
+	rpio "github.com/stianeikeland/go-rpio/v4"
 )
 
 func main() {
 	fmt.Printf("Starting traffic lights at %s\n", time.Now())
 
 	if err := rpio.Open(); err != nil {
+
+		fmt.Printf("Cannot access GPIO: %s\n", time.Now())
+
 		fmt.Println(err)
 		os.Exit(1)
 	}
@@ -23,10 +26,14 @@ func main() {
 	yellowPin := rpio.Pin(3)
 	greenPin := rpio.Pin(4)
 
+	fmt.Printf("GPIO input set up: %s\n", time.Now())
+
 	// Set the pins to output mode
 	redPin.Output()
 	yellowPin.Output()
 	greenPin.Output()
+
+	fmt.Printf("GPIO output set up: %s\n", time.Now())
 
 	// Clean up on ctrl-c and turn lights out
 	c := make(chan os.Signal, 1)
@@ -54,6 +61,8 @@ func main() {
 
 	// A while true loop.
 	for {
+		fmt.Println("\tSwitching lights on and off")
+
 		// Red
 		redPin.High()
 		time.Sleep(time.Second * 2)
