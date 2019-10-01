7a2662ddf807c2a4a757a33e2739f73ee291dd0f
Tamara Kaufler <tkaufler@gmail.com>
2019-10-01 23:15:12 +0100

    adds script for rescheduling the traffic lights pod

---
+++ start_stop_pod.sh
@@ -0,0 +1,27 @@
+kubectl apply -f traffic_lights_pod.yaml
+sleep 6
+kubectl delete pod traffic-lights
+sleep 3
+kubectl apply -f traffic_lights_pod.yaml
+sleep 6
+kubectl delete pod traffic-lights
+sleep 3
+kubectl apply -f traffic_lights_pod.yaml
+sleep 6
+kubectl delete pod traffic-lights
+sleep 3
+kubectl apply -f traffic_lights_pod.yaml
+sleep 6
+kubectl delete pod traffic-lights
+sleep 3
+kubectl apply -f traffic_lights_pod.yaml
+sleep 6
+kubectl delete pod traffic-lights
+sleep 3
+kubectl apply -f traffic_lights_pod.yaml
+sleep 6
+kubectl delete pod traffic-lights
+sleep 3
+kubectl apply -f traffic_lights_pod.yaml
+sleep 6
+kubectl delete pod traffic-lights
