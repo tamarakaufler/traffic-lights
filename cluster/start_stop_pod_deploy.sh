50233c042894fc3a2c9ee2f34e1526c5fbca080c
Tamara Kaufler <tkaufler@gmail.com>
2019-10-10 19:16:01 +0100

    minor adjustment of the deployment script

--- cluster/start_stop_pod_deploy.sh
+++ cluster/start_stop_pod_deploy.sh
@@ -5,12 +5,13 @@ echo ""
 echo "Start ..."
 sleep 5
 kubectl apply -f traffic_lights_deploy.yaml
+sleep 3
 
-for i in 1 2 3 4 5 6 7 8 9 10
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
     echo "round $i"
     echo "creating pod at `date`"
-    sleep 4
+    sleep 3
     echo "deleting pod at `date`"
     kubectl delete pod -l 'app=traffic-lights'
 done
